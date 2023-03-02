3511ac11183fa3845f351cc8fb27adb91e529d78
blaizans <99187215+blaizans@users.noreply.github.com>
2023-03-02 10:15:17 +0100

    Commit

---
+++ Shared/Todo.cs
@@ -0,0 +1,15 @@
+﻿namespace Shared;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public User Owner {get;}
+    public string Ttile { get; }
+    public bool IsCompleted { get; }
+
+    public Todo(User owner, string title)
+    {
+        Owner = owner;
+        Title = title;
+    }
+}
